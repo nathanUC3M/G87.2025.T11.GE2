76399ba3ca123fd3474efd673b77a628eb9a7198
nathanUC3M <100559120@alumnos.uc3m.es>
2025-02-20 15:39:49 +0100

    Add files via upload

---
+++ G87.2025.T11.GE2-main/src/main/python/uc3m_money/account_manager.py
@@ -0,0 +1,12 @@
+"""Module """
+
+class AccountManager:
+    """Class for providing the methods for managing the orders"""
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def validate_iban(iban: str):
+        """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
+        OR FALSE IN OTHER CASE"""
+        return True
