20211e01c1bbba42c48da6c775ed1c60e8134634
Greta Leege <100559009@alumnos.uc3m.es>
2025-03-25 09:51:55 +0100

    pylint changes

---
+++ G87.2025.T11.GE2-main/src/uc3m_money/__init__.py
@@ -0,0 +1,6 @@
+"""UC3M LOGISTICS MODULE WITH ALL THE FEATURES REQUIRED FOR ACCESS CONTROL"""
+
+from .transfer_request import TransferRequest
+from .account_manager import AccountManager
+from .account_management_exception import AccountManagementException
+from .account_deposit import AccountDeposit
